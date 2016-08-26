77e265cd19ad6c0a037fd1b1ce24b3a7a11e08bd
Johann du Toit <exxonno@gmail.com>
2016-08-26 08:17:28 +0200

    Updated to actually listen to the partial keywords

--- test/spellchecking.check.js
+++ test/spellchecking.check.js
@@ -11,6 +11,168 @@ describe('spellchecking', function() {
   describe('#check', function() { 
 
     // handle the error output
+    it('should not return the error if the mistake is contained in keyword', function(done) {
+
+      // read in the html sample
+      var content = fs.readFileSync('./samples/spellcheck.wrong.txt').toString('utf8');
+
+      // run the rules
+      spellCheck.check({ 
+
+        content:    content.split('\n'),
+        language:   'en',
+        keywords:   [ 'blokshouldskip.com' ]
+
+      } , function(err, mistakes) {
+
+        // check if defined
+        if(err) assert.fail('Spellcheck function returned a error')
+        if(!mistakes) assert.fail('Spelling mistakes returned was null/undefined')
+
+        // get the find
+        var spellingCheck = _.find(mistakes || [], function(item) {
+
+          return item.word === 'blok';
+
+        });
+
+        if(spellingCheck) {
+
+          // fail
+          assert.fail('Should not report the error');
+
+        }
+
+        // get the find
+        var spellingChecker = _.find(mistakes || [], function(item) {
+
+          return item.word === 'tht';
+
+        });
+
+        if(!spellingChecker) {
+
+          // fail
+          assert.fail('Should still report the *tht* mistake as a error');
+
+        }
+
+        // done
+        done()
+
+      });
+
+    });
+
+    // handle the error output
+    it('should not return the error if is fully in the list of keywords', function(done) {
+
+      // read in the html sample
+      var content = fs.readFileSync('./samples/spellcheck.wrong.txt').toString('utf8');
+
+      // run the rules
+      spellCheck.check({ 
+
+        content:    content.split('\n'),
+        language:   'en',
+        keywords:   [ 'tht' ]
+
+      } , function(err, mistakes) {
+
+        // check if defined
+        if(err) assert.fail('Spellcheck function returned a error')
+        if(!mistakes) assert.fail('Spelling mistakes returned was null/undefined')
+
+        // get the find
+        var spellingCheck = _.find(mistakes || [], function(item) {
+
+          return item.word === 'blok';
+
+        });
+
+        if(!spellingCheck) {
+
+          // fail
+          assert.fail('Should still give *blok* the error');
+
+        }
+
+        // get the find
+        var spellingChecker = _.find(mistakes || [], function(item) {
+
+          return item.word === 'tht';
+
+        });
+
+        if(spellingChecker) {
+
+          // fail
+          assert.fail('Should not report the *tht* mistake as a error');
+
+        }
+
+        // done
+        done()
+
+      });
+
+    });
+
+    // handle the error output
+    it('should not return the error if is fully in the list of keywords', function(done) {
+
+      // read in the html sample
+      var content = fs.readFileSync('./samples/spellcheck.wrong.txt').toString('utf8');
+
+      // run the rules
+      spellCheck.check({ 
+
+        content:    content.split('\n'),
+        language:   'en',
+        keywords:   [ 'blok' ]
+
+      } , function(err, mistakes) {
+
+        // check if defined
+        if(err) assert.fail('Spellcheck function returned a error')
+        if(!mistakes) assert.fail('Spelling mistakes returned was null/undefined')
+
+        // get the find
+        var spellingCheck = _.find(mistakes || [], function(item) {
+
+          return item.word === 'blok';
+
+        });
+
+        if(spellingCheck) {
+
+          // fail
+          assert.fail('Should not report the *blok* mistake as a error');
+
+        }
+
+        // get the find
+        var spellingChecker = _.find(mistakes || [], function(item) {
+
+          return item.word === 'tht';
+
+        });
+
+        if(!spellingChecker) {
+
+          // fail
+          assert.fail('Should still give *tht* the error');
+
+        }
+
+        // done
+        done()
+
+      });
+
+    });
+
+    // handle the error output
     it('should not report a spelling mistake', function(done) {
 
       // read in the html sample
